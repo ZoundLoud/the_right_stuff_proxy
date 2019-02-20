d9cd845bdf4804e55a06350ab47d971231242948
Amber Sharma <shrama.amber.p@gmail.com>
2019-02-19 16:21:15 -0800

    Fixed proxy server to serve up right bar components

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var cors = require('cors');
+var path = require('path');
+var morgan = require("morgan");
+// var bodyparser = require('body-parser');
+var app = express();
+
+const port = process.env.PORT || 3003;
+
+app.use(morgan('dev'));
+app.use(cors());
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`)
+});
+
